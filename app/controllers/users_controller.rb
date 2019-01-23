0a23c9bb584e0a0e9a12afe894fe8b74248cde0f
Rachel Wyatt <rw126@evansville.edu>
2019-01-23 13:29:49 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,6 +36,11 @@ require 'pry'
     end
   end
 
+  get '/users/:slug' do
+  @user = User.find_by_slug(params[:slug])
+  erb :'users/show'
+end
+
   get '/logout' do
     if logged_in?
       session.clear
