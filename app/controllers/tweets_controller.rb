4d223f5f91893280882755739c3323941e41b8cc
Rachel Wyatt <rw126@evansville.edu>
2019-01-23 13:39:36 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -39,7 +39,7 @@ class TweetsController < ApplicationController
   get '/tweets/:id/edit' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
-      erb :'/tweets/edit_tweet'
+      erb :'/tweets/edit'
     else
       redirect "/login"
     end
