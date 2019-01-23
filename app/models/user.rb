a4ecae719959fe673ef05a81c2886140d80c3ae4
Rachel Wyatt <rw126@evansville.edu>
2019-01-23 13:25:11 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :username, :email, :password_digest
 
+  def slug 
+    username.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|username| username.slug == slug}
+  end
 end
